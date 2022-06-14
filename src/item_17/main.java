ca8d8e10b4b48d73aaa21f74f55baff7800543d1
aengun <aengun@icloud.com>
2022-06-14 23:24:55 +0900

    item 18.

--- src/item_17/main.java
+++ src/item_17/main.java
@@ -7,7 +7,7 @@ public class main {
     public static void main(String[] args) {
 
         InstrumentedHashSet<String> s = new InstrumentedHashSet<>();
-        s.addAll(List.of("틱틱", "탁탁", "펑"));
+        s.addAll(List.of("틱틱", "탁탁", "펑", "a", "11"));
         System.out.println(s.toString());
         System.out.println(s.size());
         System.out.println(s.getAddCount());
---
+++ src/item_18/Super.java
@@ -0,0 +1,13 @@
+package item_18;
+
+public class Super {
+
+    public Super() {
+        overrideMe();
+    }
+
+    private void overrideMe() {
+
+    }
+
+}
